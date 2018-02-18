feaba0f01f34e628ca19edf38e76eacc1205a710
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-18 07:55:48 +0000

    about

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This app uses React and redux with es6</p>
+        </div>
+    );
+};
+
+export default AboutPage;
