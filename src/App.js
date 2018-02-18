902e4d0323bf092cf405827ad43094a50fc0940c
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-18 08:31:46 +0000

    app shows homepage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
+import HomePage from "./components/home/HomePage";
 
 class App extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <p>Header here...</p>
-                {this.props.children}
+            <div>
+                <HomePage/>
             </div>
         );
-    }
+    };
 }
 
-App.propTypes = {
-    children: PropTypes.object.isRequired
-};
 
 export default App;
