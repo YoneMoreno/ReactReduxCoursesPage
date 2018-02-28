96a93444e459cf5614bd18fd2cc5b939ee0251ab
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-28 08:28:55 +0000

    Add inmutability to our first reducer:
    export default function courseReducer(state = [], action) {
        switch (action.type) {
            case 'CREATE_COURSE':
                return [...state,
                    Object.assign({}, action.course)   <-------------------------- take the old state's contents and add a copy of the course which is a new array
                ];
                break;
            default:
                return state;
                break;
        }
    }

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,8 +1,9 @@
 export default function courseReducer(state = [], action) {
     switch (action.type) {
         case 'CREATE_COURSE':
-            state.push(action.course);
-            return state;
+            return [...state,
+                Object.assign({}, action.course)
+            ];
             break;
         default:
             return state;
