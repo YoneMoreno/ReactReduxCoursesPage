ac0dd28bd9191dc20d0468c88b24086d402048f9
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-28 09:22:26 +0000

    Add inmutability invariant to store:
    import {applyMiddleware, createStore} from 'redux';
    import rootReducer from '../reducers/index';
    import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
    
    export default function configureStore(initialState) {
        return createStore(
            rootReducer,
            initialState,
            applyMiddleware(reduxImmutableStateInvariant())           <-----------------------------------
        );
    };

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from '../reducers/index';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-    )
+        applyMiddleware(reduxImmutableStateInvariant())
+    );
 };
